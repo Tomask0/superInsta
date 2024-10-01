c59c49eabbe02a536a9e340d6826d958e0fab8d4
Tomask0 <tomas.grosek22@gmail.com>
2024-10-01 11:03:58 +0200

    initial

---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,18 @@
+
+// src/app/o-mne/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata ={ title: "o-mne | SuperApka"}
+
+export default function home() {
+
+  return (
+    
+    <Container>
+      <Typography> Stránka o mne</Typography>
+    </Container>
+
+  );
+}
---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,17 @@
+
+// src/app/podmienky/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata ={ title: "Podmienky | SuperApka"}
+
+export default function home() {
+
+  return (
+    <Container>
+      <Typography> Podmienky používania</Typography>
+    </Container>
+
+  );
+}
